5bd6f9430ffea9e60d0aee1297f7ec04e6cfd865
Ethan <ethan@etekweb.net>
2020-10-12 11:40:37 -0400

    better comments

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -27,18 +27,38 @@ class Map extends StatelessWidget {
             ),
           ],
         ),
+
+        // Begin Heatmap Code
+
+        // Each layer has two transparent circles inside of each other
+        // This creates the effect of a heatmap
+
+        // Example Layer 1: Walmart
         new CircleLayerOptions(circles: [
           new CircleMarker(
+            // Lat-long should be the same for both CircleMarkers in the layer
             point: LatLng(47.106630, -88.589029),
+            // The radius of the outer circle (should be larger than inner)
             radius: 26.0,
+            // Each color is in the format 0xAARRGGBB
+            // This allows for opacity to be changed
+            // You can find appropriate color values using this tool:
+            // http://peteroupc.github.io/colorpicker/demo.html
+            // Use the fourth item down on the list
             color: Color(0x70FF9504),
           ),
           new CircleMarker(
+            // Lat-long should be the same for both CircleMarkers in the layer
             point: LatLng(47.106630, -88.589029),
+            // The radius of the inner circle (should be larger than outer)
             radius: 19.0,
+            // See color notes above
             color: Color(0x70FF9504),
           ),
         ]),
+
+        // Example Layer 2: Applebees
+        // See comments on Example Layer 1 for details
         new CircleLayerOptions(circles: [
           new CircleMarker(
             point: LatLng(47.108655, -88.588764),
@@ -51,6 +71,9 @@ class Map extends StatelessWidget {
             color: Color(0x70ffee04),
           ),
         ]),
+
+        // Example Layer 3: Gas station
+        // See comments on Example Layer 1 for details
         new CircleLayerOptions(circles: [
           new CircleMarker(
             point: LatLng(47.108005, -88.589118),
@@ -63,6 +86,8 @@ class Map extends StatelessWidget {
             color: Color(0x70ffee04),
           ),
         ]),
+
+        // End heatmap code
       ],
     );
   }
